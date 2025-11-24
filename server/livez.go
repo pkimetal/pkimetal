d049e1f977a1b5d4dbc8bf6eccc26a0726b0510f
Rob Stradling <rob@sectigo.com>
2025-11-24 16:47:18 +0000

    Change the log level for the livez, readyz, and metrics endpoints from 'info' to 'debug'.

--- server/livez.go
+++ server/livez.go
@@ -14,7 +14,7 @@ import (
 )
 
 func livez(ctx *fasthttp.RequestCtx) int {
-	ctx.SetUserValue("level", zap.InfoLevel)
+	ctx.SetUserValue("level", zap.DebugLevel)
 	ctx.SetUserValue("msg", "Liveness check")
 
 	ctxWithDeadline, cancel := context.WithDeadline(context.Background(), ctx.Time().Add(time.Duration(config.Config.Server.LivezTimeout)))
