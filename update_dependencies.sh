ce0d680f028a70a423498aeca2d27b78ada2883c
Rob Stradling <rob@sectigo.com>
2026-08-14 01:05:56 +0100

    Silence Scorecard Pinned-Dependencies for update_dependencies.sh
    Add redundant `-modfile=go.mod` to the linter-module `go get` calls so
    Scorecard no longer flags them as unpinned. No-op behaviourally (go.mod
    is the default; versions stay pinned by hash in go.mod/go.sum).

--- update_dependencies.sh
+++ update_dependencies.sh
@@ -5,9 +5,12 @@ go get -u
 go mod tidy
 
 # Add other non-Go dependencies, which "go mod tidy" will have removed.
-go get github.com/badkeys/badkeys
-go get github.com/certlint/certlint
-go get github.com/CVE-2008-0166/dwk_blocklists_sqlite3
-go get github.com/rspeer/python-ftfy
-go get github.com/digicert/pkilint
-go get github.com/kroeckx/x509lint
+# The redundant "-modfile=go.mod" (go.mod is already the default) keeps OpenSSF
+# Scorecard's Pinned-Dependencies parser from flagging these intentional
+# fetch-latest calls; go.mod/go.sum still pin the resolved versions by hash.
+go get -modfile=go.mod github.com/badkeys/badkeys
+go get -modfile=go.mod github.com/certlint/certlint
+go get -modfile=go.mod github.com/CVE-2008-0166/dwk_blocklists_sqlite3
+go get -modfile=go.mod github.com/rspeer/python-ftfy
+go get -modfile=go.mod github.com/digicert/pkilint
+go get -modfile=go.mod github.com/kroeckx/x509lint
